1f0d402fa5023b243943dc384bd3868196d02957
kmspeter <kmspeter@naver.com>
2025-10-06 14:58:04 +0900

    20251006_v5

--- src/config/api.js
+++ src/config/api.js
@@ -15,10 +15,3 @@ export const resolveApiUrl = (path = '') => {
   const normalizedPath = path.startsWith('/') ? path : `/${path}`;
   return `${API_BASE_URL}${normalizedPath}`;
 };
-
-export const resolveIpfsUrl = (path = '') => {
-  if (!path) return IPFS_BASE_URL;
-  if (/^https?:\/\//i.test(path)) return path;
-  const normalizedPath = path.startsWith('/') ? path : `/${path}`;
-  return `${IPFS_BASE_URL}${normalizedPath}`;
-};
